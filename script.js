8e950f9e5ffff36fca4216e967189b2fdc59c99d
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-17 03:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let cnt=0;
+
+let p = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click",()=>{
+    alert(cnt);
+    cnt++;
+    p.textContent = `${cnt}`;
+})
